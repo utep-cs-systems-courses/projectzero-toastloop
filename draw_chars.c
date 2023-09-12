3b30722ca41c9870e82cfae570ea48c413abfa08
toastloop <69254179+toastloop@users.noreply.github.com>
2023-09-11 21:00:14 -0600

    Fixed the order of the columns for the 8x12 font

--- draw_chars.c
+++ draw_chars.c
@@ -18,7 +18,9 @@ void print_char_8x12(char c)
   c -= 0x20;
   for (char row = 0; row < 8; row++) {
     for (char col = 0; col < 12; col++) {
-      unsigned char colBits = font_8x12[c][col];
+      unsigned char colBits = font_8x12[c][12-col]; 
+      /* 12 - col flips the column order */
+      /* 1 << row flips the row order */
       unsigned short rowMask = 1 << (8-row); /* mask to select bit associated with bit */
       putchar( (colBits & rowMask) ? '*' : ' ');
     }
