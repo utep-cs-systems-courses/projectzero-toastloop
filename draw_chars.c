b43df1bb3c60d7f9b5b24b64982650ebf8e282a4
toastloop <69254179+toastloop@users.noreply.github.com>
2023-09-11 19:29:23 -0600

    Added print_char_8x12

--- draw_chars.c
+++ draw_chars.c
@@ -13,3 +13,15 @@ void print_char_11x16(char c)
     putchar('\n');
   }
 }
+void print_char_8x12(char c)
+{
+  c -= 0x20;
+  for (char row = 0; row < 8; row++) {
+    for (char col = 0; col < 12; col++) {
+      unsigned char colBits = font_8x12[c][col];
+      unsigned short rowMask = 1 << (8-row); /* mask to select bit associated with bit */
+      putchar( (colBits & rowMask) ? '*' : ' ');
+    }
+    putchar('\n');
+  }
+}
