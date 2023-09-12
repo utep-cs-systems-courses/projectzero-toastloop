04adfb1a987059ca078dc86370af2e9cc2c9cb02
toastloop <69254179+toastloop@users.noreply.github.com>
2023-09-11 19:29:36 -0600

    Added print_arrow

--- draw_shapes.c
+++ draw_shapes.c
@@ -26,3 +26,9 @@ void print_triangle(int leftCol, int size)
   }
 }
 
+// Prints an arrow of specified height whose left edge is at col leftCol.
+void print_arrow(int leftCol, int size)
+{
+  print_triangle(leftCol, size);
+  print_square((leftCol+3), size);
+}
