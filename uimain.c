2500d267d314bf53519ea7ea62347059a44f7484
toastloop <69254179+toastloop@users.noreply.github.com>
2023-09-13 15:38:05 -0600

    Added an option for 5x7 font

--- uimain.c
+++ uimain.c
@@ -7,7 +7,7 @@ int main()
 
   while (1) { // Infinite while loop
 
-    fputs("Select which shape you want to print (Triangle = t, Square = s, Arrow = a, 11x16 Chars = c, 8x12 Chars = d) or 'q' to quit\n> ", stdout);
+    fputs("Select which shape you want to print (Triangle = t, Square = s, Arrow = a, 11x16 Chars = c, 8x12 Chars = d, 5x7 Chars = e) or 'q' to quit\n> ", stdout);
     fflush(stdout);		/* stdout only flushes automatically on \n */
     int c;
     while ((c = getchar()) == '\n'); /* ignore newlines */
@@ -37,6 +37,11 @@ int main()
       puts("You selected 8x12 chars:");
       for (char c = 'A'; c < 'D'; c++)
         print_char_8x12(c);
+      break;
+    case 'e':
+      puts("You selected 5x7 chars:");
+      for (char c = 'A'; c < 'D'; c++)
+        print_char_5x7(c);
       break;
     case 'q':
       puts("Bye!");
