9f67eb454630893384bbc4ba7c1ae64c4e7613fc
toastloop <69254179+toastloop@users.noreply.github.com>
2023-09-13 15:37:17 -0600

    Added print_char_5x7

--- draw_chars.c
+++ draw_chars.c
@@ -1,28 +1,39 @@
-#include "draw.h"		/* for font */
-#include "stdio.h"		/* for putchar */
+#include "draw.h"  /* for font */
+#include "stdio.h" /* for putchar */
 
-void print_char_11x16(char c)
-{
+void print_char_11x16(char c) {
   c -= 0x20;
   for (char row = 0; row < 11; row++) {
     unsigned short rowBits = font_11x16[c][row];
     for (char col = 0; col < 16; col++) {
-      unsigned short colMask = 1 << (15-col); /* mask to select bit associated with bit */
-      putchar( (rowBits & colMask) ? '*' : ' ');
+      unsigned short colMask =
+          1 << (15 - col); /* mask to select bit associated with bit */
+      putchar((rowBits & colMask) ? '*' : ' ');
     }
     putchar('\n');
   }
 }
-void print_char_8x12(char c)
-{
+void print_char_8x12(char c) {
   c -= 0x20;
   for (char row = 0; row < 8; row++) {
     for (char col = 0; col < 12; col++) {
-      unsigned char colBits = font_8x12[c][12-col]; 
+      unsigned char colBits = font_8x12[c][12 - col];
       /* 12 - col flips the column order */
       /* 1 << row flips the row order */
-      unsigned short rowMask = 1 << (8-row); /* mask to select bit associated with bit */
-      putchar( (colBits & rowMask) ? '*' : ' ');
+      unsigned short rowMask =
+          1 << (8 - row); /* mask to select bit associated with bit */
+      putchar((colBits & rowMask) ? '*' : ' ');
+    }
+    putchar('\n');
+  }
+}
+void print_char_5x7(char c) {
+  c -= 0x20;
+  for (char row = 0; row < 5; row++) {
+    unsigned short rowBits = font_5x7[c][row];
+    for (char col = 0; col < 8; col++) {
+      unsigned short colMask = 1 << (7 - col);
+      putchar((rowBits & colMask) ? '*' : ' ');
     }
     putchar('\n');
   }
